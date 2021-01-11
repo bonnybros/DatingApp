ce0abd3f8527bc1c9dfc398f61c9261f1cfc30a2
Bonny <bonny.bros@gmail.com>
2021-01-11 07:46:35 -0700

    Before updating Angular Version to fix ambient context error

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 //import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
@@ -18,7 +23,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +35,9 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ToastrModule.forRoot({
+        positionClass: 'toast-bottom-right'
+    })
     //BsDropdownModule.forRoot()
 
   ],
